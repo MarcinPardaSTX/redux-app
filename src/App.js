9370b5aaea1a3b55dd81aa0fab2f0ae21a5d8f67
Marcin Parda <marcin.parda@stxnext.pl>
2022-04-20 18:50:46 +0200

    setup project for redux toolkit tutorial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
-import './App.css';
-
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
-  const increment = () => dispatch({ type: 'INCREMENT' });
-  const decrement = () => dispatch({ type: 'DECREMENT' });
-  const addBy = () => dispatch({ type: 'ADD', payload: 10});
   return (
     <div>
       <h1>Counter App</h1>
-      <h2>{counter}</h2>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={addBy}>Add Value</button>
     </div>
   );
 }
