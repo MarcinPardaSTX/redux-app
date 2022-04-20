f834a317eff83c03b79995e5054e34ca4c4abf55
Marcin Parda <marcin.parda@stxnext.pl>
2022-04-20 15:00:36 +0200

    added increment and decrement dispatches

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,18 @@
 import { createStore } from 'redux';
 
-const reducerFn = (state = { counter: 0 }, action) => {};
+const reducerFn = (state = { counter: 0 }, action) => {
+  if (action.type === 'INCREMENT') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === 'DECREMENT') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
 
 const store = createStore(reducerFn);
 
